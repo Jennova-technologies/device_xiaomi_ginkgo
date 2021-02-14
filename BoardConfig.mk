dab7fd34235cf920c23fc4f8c7ab1b34c6108817
Jennova-technologies <jennova.technologies@gmail.com>
2021-02-14 20:26:51 +0100

    Use jennova_defconfig for kernel configuration

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -125,7 +125,7 @@ BOARD_RAMDISK_OFFSET := 0x01000000
 BOARD_KERNEL_SEPARATED_DTBO := true
 
 TARGET_KERNEL_SOURCE := kernel/xiaomi/ginkgo
-TARGET_KERNEL_CONFIG := vendor/ginkgo-perf_defconfig
+TARGET_KERNEL_CONFIG := vendor/jennova_defconfig
 
 TARGET_KERNEL_CLANG_COMPILE := true
 TARGET_KERNEL_CLANG_VERSION := sdllvm
